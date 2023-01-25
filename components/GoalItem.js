7ba754edc5a38ca3e2137fd8060634d6e99c0764
Burak Kahraman <107571356+BurakKahraman1@users.noreply.github.com>
2023-01-25 20:15:37 +0300

    Update GoalItem.js

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,14 +2,14 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 let GoalItem = (props) => {
   return (
-    //vieve onpress verilemedigi icin pressable componentini kullanicaz
+    
     <View style={styles.goalItem}>
       <Pressable
         android_ripple={{ color: "#dddddd" }}
         onPress={() => props.onDeleteGoal(props.id)}
         style={({pressed})=> pressed && styles.pressedItem}
       >
-    {/* android icin riplle kullandik iosda efekt icin style ile pressablein sagladigi props.pressed i {pressed} diy aldik */}
+   
         <Text style={{ color: "white", padding: 8, }}>{props.value}</Text>
       </Pressable>
     </View>
